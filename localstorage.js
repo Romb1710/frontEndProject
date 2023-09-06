1c293eee8ceaefad66af710ca37eccbbc4cc448e
Romb1710 <91134640+Romb1710@users.noreply.github.com>
2023-09-06 17:05:42 +0300

    Add files via upload

---
+++ localstorage.js
@@ -0,0 +1,21 @@
+/*
+// Save data to local storage using async method
+export const saveData = async (key, value) => {
+  let newCosts = await getData(key);
+  newCosts = newCosts.concat(value)
+  localStorage.setItem(key, JSON.stringify(newCosts));
+};
+
+// Get data from local storage using async method
+export const getData = async (key) => {
+  return new Promise((resolve, reject) => {
+    try {
+      let data = localStorage.getItem(key);
+      resolve(JSON.parse(data) || []);
+    } catch (error) {
+      reject("No data found");
+    }
+  });
+};
+
+*/
